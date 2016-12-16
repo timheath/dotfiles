63d8e6c55d99c06046e296ed08bd36162eb74309
Tim Heath <github@timheath.org>
2016-12-15 20:45:06 -0800

    added golang path to env

--- system/path.sh
+++ system/path.sh
@@ -5,21 +5,19 @@ is-executable getconf && PATH=$(command -v getconf PATH)
 
 # Prepend new items to path (if directory exists)
 
-#/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/opt/coreutils/libexec/gnubin:/Users/xbbl4bt/dev/golang/bin:/Users/xbbl4bt/dev/golang/bin
-
 # redhat specific path elements
 if [ -f /etc/redhat-release ]; then
     prepend-path "/usr/seos/bin"
     prepend-path "/usr/lib64/qt-3.3/bin"
 fi
 
-#export PATH=$PATH:$GOPATH/bin
-#export GOPATH=$HOME/dev/golang
+export GOPATH=$HOME/dev/golang
 
 prepend-path "/bin"
 prepend-path "/usr/bin"
-prepend-path "$HOME/dev/golang/bin"
+prepend-path "$GOPATH/bin"
 prepend-path "/usr/local/bin"
+prepend-path "/usr/local/go/bin"
 is-executable brew && prepend-path "$(brew --prefix coreutils)/libexec/gnubin"
 prepend-path "$DOTFILES_DIR/bin"
 prepend-path "$HOME/bin"
