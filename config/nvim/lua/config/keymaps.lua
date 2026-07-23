e4dbdec557b4cfd239b5e4d6d96ee7edc5dd6f02
Tim Heath <github@timheath.org>
2026-07-23 18:36:27 -0500

    bind window scroll to ctrl-j/k

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", ";", ":", { desc = "Command-line" })
+
+-- Scroll viewport without moving cursor (overrides LazyVim window nav on C-j/C-k)
+vim.keymap.set("n", "<C-j>", "<C-e>", { desc = "Scroll down" })
+vim.keymap.set("n", "<C-k>", "<C-y>", { desc = "Scroll up" })
